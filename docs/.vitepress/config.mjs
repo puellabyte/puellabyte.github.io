9d9c4fcf2895923a06c86f10c513e5190f230a64
Curstantine <curstantine@proton.me>
2025-05-15 15:25:39 +0530

    chore: cleanup and update build to use pnpm

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,37 +2,37 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "同人音楽 - Event Dates",
-  description: "lists 同人 event dates where music was distributed",
-  cleanUrls: true,
-  themeConfig: {
-    search: {
-      provider: "local",
-    },
+	title: "同人音楽 - Event Dates",
+	description: "lists 同人 event dates where music was distributed",
+	cleanUrls: true,
+	themeConfig: {
+		search: {
+			provider: "local",
+		},
 
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: "Home", link: "/" },
-      { text: "Events", link: "/events" },
-    ],
+		// https://vitepress.dev/reference/default-theme-config
+		nav: [
+			{ text: "Home", link: "/" },
+			{ text: "Events", link: "/events" },
+		],
 
-    sidebar: [
-      {
-        text: "Events",
-        items: [{ text: "Doujin Events", link: "/events" }],
-      },
-    ],
+		sidebar: [
+			{
+				text: "Events",
+				items: [{ text: "Doujin Events", link: "/events" }],
+			},
+		],
 
-    socialLinks: [
-      {
-        icon: "github",
-        link: "https://github.com/puellabyte/puellabyte.github.io/",
-      },
-    ],
-  },
-  markdown: {
-    image: {
-      lazyLoading: true,
-    },
-  },
+		socialLinks: [
+			{
+				icon: "github",
+				link: "https://github.com/puellabyte/puellabyte.github.io/",
+			},
+		],
+	},
+	markdown: {
+		image: {
+			lazyLoading: true,
+		},
+	},
 });
--- docs/index.md
+++ docs/index.md
@@ -2,14 +2,14 @@
 layout: home
 
 hero:
-  name: doujin events
-  text: events and things.
-  tagline: i dont know what im doing please help me
-  actions:
-    - theme: brand
-      text: Events
-      link: /events
-    - theme: alt
-      text: Discord
-      link: https://discord.gg/doujincafe
+    name: doujin events
+    text: events and things.
+    tagline: i dont know what im doing please help me
+    actions:
+        - theme: brand
+          text: Events
+          link: /events
+        - theme: alt
+          text: Discord
+          link: https://discord.gg/doujincafe
 ---
